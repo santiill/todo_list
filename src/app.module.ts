71c4ea2871784a826d90e098960d7903fc5ed2fb
santiill <szebiniso0909@gmail.com>
2022-11-13 20:40:19 +0600

    added files to gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+import { DB_CONFIG } from './utils/db_config';
 import { DB_ENTITIES } from "./utils/db_entities";
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '12345',
-    database: 'todo_list',
-    entities: DB_ENTITIES,
-    synchronize: true,
-  }), TasksModule],
+  imports: [TypeOrmModule.forRoot(DB_CONFIG), TasksModule],
   controllers: [],
   providers: [],
 })
